8ee08d6ea5622740d2aaaacd1e0a5d63af2e6e00
MFBarreto <marcus1fialho.gmail.com>
2025-07-03 16:12:48 -0300

    Refatoração: Implementa o Vite para o processo de build

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Diga ao Vite que a raiz do seu projeto é a pasta 'src'.
+  root: 'src',
+
+  build: {
+    // Diga ao Vite para colocar o resultado do build na pasta 'dist' na raiz do projeto.
+    outDir: '../dist',
+    // Limpa o diretório de build antes de compilar.
+    emptyOutDir: true,
+  },
+});
