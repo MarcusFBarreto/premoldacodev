cefc3dfc2ee14356b39294257cf3940c0cdbb534
MFBarreto <marcus1fialho.gmail.com>
2025-07-24 20:25:50 -0300

    5 - Ajustes para SEO, Analytcs e coisas do tipo - 24/07/25

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-// vite.config.js - VERSÃO FINAL E CORRETA
+// vite.config.js - VERSÃO CORRIGIDA E FINAL
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   // Define a pasta 'src' como a raiz do nosso site para o Vite
   root: 'src',
+
+  // Diz ao Vite que a pasta public está um nível ACIMA da raiz 'src'
+  publicDir: '../public',
 
   build: {
     // Define o diretório de saída como 'dist' na raiz do projeto
@@ -15,9 +18,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'src/index.html'),
         calculadora: resolve(__dirname, 'src/calculadora.html'),
-        pushin: resolve(__dirname, 'src/pushin.html'), // <-- ADICIONE ESTA LINHA
-        admin: resolve(__dirname, 'src/admin.html'), // <-- ADICIONE ESTA LINHA
-        404: resolve(__dirname, 'src/404.html'),
+        pushin: resolve(__dirname, 'src/pushin.html'),
+        admin: resolve(__dirname, 'src/admin.html'),
+        '404': resolve(__dirname, 'src/404.html'), // Corrigido para ser uma string válida
       },
     },
   },
